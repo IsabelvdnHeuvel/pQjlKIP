5f36fb56374fcb24293a8e1874d0527f1244b46e
IsabelvdnHeuvel <38749695+IsabelvdnHeuvel@users.noreply.github.com>
2019-03-08 18:10:54 +0100

    Add files via upload

---
+++ copy.sql
@@ -0,0 +1,12 @@
+
+COPY Degrees (DegreeID, Dept, DegreeDescription, TotalECTS) FROM '/mnt/ramdisc/tables/Degrees.table' DELIMITER ',' CSV HEADER;
+COPY Students (StudentID, StudentName, Address, BirthyearStudent, Gender) FROM '/mnt/ramdisc/tables/Students.table' DELIMITER ',' CSV HEADER;
+COPY StudentRegistrationsToDegrees (StudentRegistrationID, StudentID, DegreeID, RegistrationYear) FROM '/mnt/ramdisc/tables/StudentRegistrationsToDegrees.table' DELIMITER ',' CSV HEADER;
+COPY Teachers (TeacherID, TeacherName, Address, BirthyearTeacher, Gender) FROM '/mnt/ramdisc/tables/Teachers.table' DELIMITER ',' CSV HEADER;
+COPY Courses (CourseID, CourseName, CourseDescription, DegreeID, ECTS) FROM '/mnt/ramdisc/tables/Courses.table' DELIMITER ',' CSV HEADER;
+COPY CourseOffers (CourseOfferID, CourseID, Year, Quartile) FROM '/mnt/ramdisc/tables/CourseOffers.table' DELIMITER ',' CSV HEADER;
+COPY TeacherAssignmentsToCourses (CourseOfferID, TeacherID) FROM '/mnt/ramdisc/tables/TeacherAssignmentsToCourses.table' DELIMITER ',' CSV HEADER;
+COPY StudentAssistants (CourseOfferID, StudentRegistrationID) FROM '/mnt/ramdisc/tables/StudentAssistants.table' DELIMITER ',' CSV HEADER;
+COPY CourseRegistrations (CourseOfferID, StudentRegistrationID, Grade) FROM '/mnt/ramdisc/tables/CourseRegistrations.table' DELIMITER ',' CSV HEADER;
+
+ANALYZE VERBOSE
---
+++ load.sh
@@ -0,0 +1,5 @@
+psql -d uni -f create.sql
+psql -d uni -f copy.sql
+psql -d uni -f index.sql
+psql -d uni -f queriesafterloading.sql
+psql -d uni -f q2Queries.sql
