7a1a855957729020490a1058d7101c11ab6f7611
IsabelvdnHeuvel <38749695+IsabelvdnHeuvel@users.noreply.github.com>
2019-03-12 13:11:08 +0100

    Update q2Create.sql

--- q2Create.sql
+++ q2Create.sql
@@ -16,6 +16,6 @@ ALTER TABLE StudentAssistants ADD FOREIGN KEY (StudentRegistrationID) REFERENCES
 ALTER TABLE CourseRegistrations ADD FOREIGN KEY (CourseOfferID) REFERENCES CourseOffers(CourseOfferID);
 ALTER TABLE CourseRegistrations ADD FOREIGN KEY (StudentRegistrationID) REFERENCES StudentRegistrationsToDegrees(StudentRegistrationID);
 CREATE INDEX idx_student ON StudentRegistrationsToDegrees(StudentRegistrationID);
-CREATE INDEX idx_course ON CouseOffers(CourseOfferID);
+CREATE INDEX idx_course ON CourseOffers(CourseOfferID);
 CREATE MATERIALIZED VIEW NewCR(StudentID, CourseOfferID, CourseID, StudentRegistrationID, Grade) AS (SELECT SRD.StudentID, CR.CourseOfferID, CO.CourseID, CR.StudentRegistrationID, CR.Grade FROM ((CourseRegistrations CR INNER JOIN CourseOffers CO ON CR.CourseOfferID = CO.CourseOfferID) INNER JOIN StudentRegistrationsToDegrees SRD ON CR.StudentRegistrationID = SRD.StudentRegistrationID));
 CREATE MATERIALIZED VIEW ECTS(StudentRegistrationID, DegreeID, sumECTS) AS (SELECT NewCR.StudentRegistrationID, SRD.DegreeID, SUM(C.ECTS) FROM NewCR, Courses C, StudentRegistrationsToDegrees SRD WHERE NewCR.Grade >= 5 AND SRD.StudentRegistrationID = NewCR.StudentRegistrationID AND C.CourseID = NewCR.CourseID GROUP BY NewCR.StudentRegistrationID, SRD.DegreeID);
